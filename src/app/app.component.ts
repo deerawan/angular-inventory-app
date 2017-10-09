50cd462240fa273c90060d16686101cee6359a57
Budi Irawan <budi.irawan@invoice2go.com>
2017-10-09 16:35:10 +0700

    feat: product list, product row

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  products: Product[];
+
+  constructor() {
+    this.products = [
+      new Product('NICEHAT', 'A Nice Black Hat',
+      '/images/product/black-hat.jpg', ['men', 'accessories', 'hats'], 29.99),
+      new Product('NEATTOJACKET', 'Hoody Jacket',
+      '/images/product/blue-jacket.jpg', ['men', 'apparel', 'jackets'], 59.99)
+    ];
+  }
+
+  productWasSelected(product: Product): void {
+    console.log(product.name);
+  }
 }
